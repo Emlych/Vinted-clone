3576c828eb4b7553070dc16935e5f04d5477c963
Emily LD <lagreouemily@gmail.com>
2022-02-15 18:36:01 +0100

    iin offer publish route : owner changed from targetUser to req.user

--- routes/offer.js
+++ routes/offer.js
@@ -36,8 +36,8 @@ const isAuthenticated = async (req, res, next) => {
 //Publish offer (create offer)
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
   console.log("route: /offer/publish");
-  const tokenUser = req.headers.authorization;
-  const targetUser = await User.findOne(tokenUser.replace("Bearer ", ""));
+  // const tokenUser = req.headers.authorization;
+  // const targetUser = await User.findOne(tokenUser.replace("Bearer ", ""));
   // const targetUser = await User.findById(req.fields.userId);
   try {
     //create new offer
@@ -53,7 +53,8 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
         { EMPLACEMENT: req.fields.city },
       ],
       //product_image: result,
-      owner: targetUser,
+      // owner: targetUser,
+      owner: req.user,
     });
 
     //upload photo on cloudinary
